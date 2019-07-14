af1f02b0660860cf6e3f58a17d52871752a89c5e
Mike <Jetmax25@gmail.com>
2019-07-13 22:39:57 -0400

    Initial Commit

---
+++ PickledSwift/PickledSwift/PickledSwift.h
@@ -0,0 +1,19 @@
+//
+//  PickledSwift.h
+//  PickledSwift
+//
+//  Created by Michael Isasi on 7/13/19.
+//  Copyright © 2019 Pickled Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PickledSwift.
+FOUNDATION_EXPORT double PickledSwiftVersionNumber;
+
+//! Project version string for PickledSwift.
+FOUNDATION_EXPORT const unsigned char PickledSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PickledSwift/PublicHeader.h>
+
+
---
+++ PickledSwift/PickledSwiftExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PickledSwiftExamples
+//
+//  Created by Michael Isasi on 7/13/19.
+//  Copyright © 2019 Pickled Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
