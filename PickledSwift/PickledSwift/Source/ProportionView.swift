d0d181383c651e345b0c9dd65df6224780403489
Mike <Jetmax25@gmail.com>
2019-12-07 17:24:21 -0500

    0.5.1

--- PickledSwift/PickledSwift/Source/ProportionView.swift
+++ PickledSwift/PickledSwift/Source/ProportionView.swift
@@ -6,24 +6,24 @@
 //  Copyright © 2019 Pickled Games. All rights reserved.
 //
 
-//public protocol Proportional {
-//    var weight: Double { get set }
-//    var key: String { get set }
-//    var order: Int { get set }
-//}
-//
-//open class ProportionView: UIView, Proportional {
-//    
-////    public var weight: Double = 1.0 {
-////        didSet {
-////            self.invalidateIntrinsicContentSize()
-////        }
-////    }
-////
-////    public var key: String = ""
-////    public var order: Int = 0
-////
-//////    final override public var intrinsicContentSize: CGSize {
-//////        return CGSize(width: weight, height: weight)
-//////    }
-//}
+public protocol Proportional {
+    var weight: Double { get set }
+    var key: String { get set }
+    var order: Int { get set }
+}
+
+open class ProportionView: UIView, Proportional {
+    
+    public var weight: Double = 1.0 {
+        didSet {
+            self.invalidateIntrinsicContentSize()
+        }
+    }
+
+    public var key: String = ""
+    public var order: Int = 0
+
+    final override public var intrinsicContentSize: CGSize {
+        return CGSize(width: weight, height: weight)
+    }
+}
--- PickledSwift/PickledSwift/Source/ProportionalStackView.swift
+++ PickledSwift/PickledSwift/Source/ProportionalStackView.swift
@@ -1,56 +1,56 @@
-////
-////  ProportionalStackView.swift
-////  PickledSwift
-////
-////  Created by Michael Isasi on 8/12/19.
-////  Copyright © 2019 Pickled Games. All rights reserved.
-////
 //
+//  ProportionalStackView.swift
+//  PickledSwift
 //
-/////Datasourace for prop view
-//public protocol ProportionViewDataSource {
-//    var propViews: [ProportionView] { get }
-//}
+//  Created by Michael Isasi on 8/12/19.
+//  Copyright © 2019 Pickled Games. All rights reserved.
 //
-/////Holds multiple views and displays them proportionally
-//public class ProportionStackView: UIView {
-//    var isOutOf100: Bool = false
-//    var viewOrder: ProportionViewOrder = .order
-//    
-//    let stack = UIStackView()
-//    
-//    var dataSource: ProportionViewDataSource? {
-//        didSet {
-//            self.reload()
-//        }
-//    }
-//    
-//    public func reload() {
-//        OperationQueue.main.addOperation {
-//            self.stack.removeAllArrangedSubviews()
-//            self.dataSource?.propViews.forEach { view in
-//                self.stack.addArrangedSubview(view)
-//            }
-//        }
-//    }
-//    
-//    public override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        self.setUp()
-//    }
-//    
-//    public required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//        self.setUp()
-//    }
-//    
-//    private func setUp() {
-//        
-//        self.addSubview(stack)
-//        stack.frame = self.bounds
-//        
-//        stack.alignment = .fill
-//        stack.distribution = .fillProportionally
-//    }
-//    
-//}
+
+
+///Datasourace for prop view
+public protocol ProportionViewDataSource {
+    var propViews: [ProportionView] { get }
+}
+
+///Holds multiple views and displays them proportionally
+public class ProportionStackView: UIView {
+    var isOutOf100: Bool = false
+    var viewOrder: ProportionViewOrder = .order
+    
+    let stack = UIStackView()
+    
+    var dataSource: ProportionViewDataSource? {
+        didSet {
+            self.reload()
+        }
+    }
+    
+    public func reload() {
+        OperationQueue.main.addOperation {
+            self.stack.removeAllArrangedSubviews()
+            self.dataSource?.propViews.forEach { view in
+                self.stack.addArrangedSubview(view)
+            }
+        }
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setUp()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setUp()
+    }
+    
+    private func setUp() {
+        
+        self.addSubview(stack)
+        stack.frame = self.bounds
+        
+        stack.alignment = .fill
+        stack.distribution = .fillProportionally
+    }
+    
+}
